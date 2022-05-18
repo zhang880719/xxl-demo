bfd76a3df5edd485c4cffb3cfbb638d391059f3d
zdj <380056768@qq.com>
2022-05-18 22:52:37 +0800

    xxl-demo init

---
+++ src/main/java/com/zdj/xxldemo/XxlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zdj.xxldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zdj/xxldemo/XxlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdj.xxldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxlDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
